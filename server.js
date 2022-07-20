72df1027e1291060102e220878c75b0e4c131c2d
Mohit Srivastava <iammohitpro@gmail.com>
2022-07-20 21:48:39 +0530

    test7

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-import express from "express";
+//import express from "express";
+const express = require("express");
 //import cors from "cors";
-import cors from "cors";
+const cors = require("cors");
 //import { readdirSync } from "fs";
-import { readdirSync } from "fs";
-import mongoose from "mongoose";
+const { readdirSync } = require("fs");
+//import mongoose from "mongoose";
+const mongoose = require("mongoose");
 //import csrf from "csurf";
-import csrf from "csurf";
+const csrf = require("csurf");
 //import cookieParser from "cookie-parser";
-import cookieParser from "cookie-parser";
+const cookieParser = require("cookie-parser");
 
-import dotenv from 'dotenv'
-import weird from `./routes/${r}`
-
-import morgan from "morgan";
-dotenv.config();
+const morgan = require("morgan");
+require("dotenv").config();
 
 const csrfProtection = csrf({ cookie: true });
 
@@ -21,7 +20,8 @@ const csrfProtection = csrf({ cookie: true });
 const app = express();
 
 //db
-connect(process.env.DATABASE, {
+mongoose
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -30,12 +30,12 @@ connect(process.env.DATABASE, {
 
 //middlewares
 app.use(cors());
-app.use(json({ limit: "5mb" }));
+app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 
 //route
-readdirSync("./routes").map((r) => app.use("/api", weird));
+readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 //csrf
 app.use(csrfProtection);
 
