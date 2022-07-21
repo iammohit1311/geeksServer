82c8187c4d8cde021c6f305e3c39fd5ef61e9bfe
Mohit Srivastava <iammohitpro@gmail.com>
2022-07-21 07:38:20 +0530

    csrf9

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const csrfProtection = csrf({ cookie: true });
 
 //create express
 const app = express();
-app.use(csrfProtection);
+
 //db
 mongoose
   .connect(process.env.DATABASE, {
@@ -33,7 +33,7 @@ app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 app.use(morgan("dev"));
-
+app.use(csrfProtection);
 //route
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 //csrf
